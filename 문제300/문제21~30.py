57be42193b426a476805315fc1c56a5cb02018e9
B10\User <junrah@nate.com>
2021-05-01 17:33:35 +0900

    2021-0501

---
+++ 문제300/문제21~30.py
@@ -0,0 +1,41 @@
+
+# 021
+letters = 'python'
+print(letters[0],letters[2])
+
+# 022
+license_plate = "24가 2210"
+print(license_plate[4:])
+
+# 023
+string = "홀짝홀짝홀짝"
+print(string[0: :2])
+
+# 024
+string = "PYTHON"
+print(string[ : :-1])
+
+# 025
+phone_number = "010-1111-2222"
+print(phone_number[0:3],phone_number[4:8],phone_number[9:13])
+
+# 026
+phone_number = "010-1111-2222"
+print(phone_number[0:3]+phone_number[4:8]+phone_number[9:13])
+
+# 027
+url = "http://sharebook.kr"
+print(url.split(".")[1])
+
+# 028
+# python
+
+# 029
+string = 'abcdfe2a354a32a'
+print(string.replace("a","A"))
+
+# 030
+string = 'abcd'
+string.replace('b','B')
+print(string)
+# abcd
