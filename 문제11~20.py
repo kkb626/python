39acd5d84fb6a334868de41477fc71830103e55e
B10\User <junrah@nate.com>
2021-05-01 15:54:19 +0900

    2021-0501

---
+++ 문제11~20.py
@@ -0,0 +1,46 @@
+
+# 011
+삼성전자 = 50000
+총평가금액 = 삼성전자*10
+print("총 평가금액 : ", 총평가금액)
+
+# 012
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print("시가총액 :", 시가총액)
+print("현재가 : ", 현재가 )
+print("PER :", PER)
+
+# 013
+s = "hello"
+t = "python"
+print(s+"!",t)
+
+# 014
+값 = 2+2*3
+print( 값 )
+
+# 015
+a = "132"
+print(type(a))
+
+# 016
+num_str = "720"
+print(int(num_str))
+
+# 017
+num = 100
+print(str(num))
+
+# 018
+print(float("15.79"))
+
+# 019
+year = "2020"
+print(int(year), int(year)-1, int(year)-2)
+
+# 020
+금액 = 48584
+총금액 = 금액*36
+print(총금액)
